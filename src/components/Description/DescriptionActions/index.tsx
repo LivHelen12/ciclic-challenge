bd870aec564b56b52c1e1a548cf776fd129794e6
Livia Helen <liviahelen025@gmail.com>
2023-08-13 11:54:38 -0300

    Create description button style.

--- src/components/Description/DescriptionActions/index.tsx
+++ src/components/Description/DescriptionActions/index.tsx
@@ -1,11 +1,12 @@
 import { HTMLAttributes } from "react";
+import "./style.scss";
 interface FormActionProps extends HTMLAttributes<HTMLButtonElement> {
   children: string;
 }
 
 export function DescriptionAction({ children, ...props }: FormActionProps) {
   return (
-    <button type="submit" className="button" {...props}>
+    <button type="submit" className="btnDescription" {...props}>
       {children}
     </button>
   );
